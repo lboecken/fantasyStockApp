5f23e604f1648f850c90ca62c7c3584ccd0c866b
Lennart Boecken <lsboecken@gmail.com>
2022-03-10 17:46:42 -0500

    fix server rename

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { render } from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Home from 'modules/home/';
 
-ReactDOM.render(
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
