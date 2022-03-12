b1ed2a7be97fcabdfb1292d221daeedaae9dc59b
Lennart Boecken <lsboecken@gmail.com>
2022-03-12 09:47:48 -0500

    feat client: add import config in jsconfig.json & vite.config

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Test from '@Common/Test';
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Test />
+    </>
+  );
+}
+
+export default App;
---
+++ client/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+const path = require('path');
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@Common': path.resolve(__dirname, '/src/modules/common'),
+      '@Home': path.resolve(__dirname, '/src/modules/home'),
+    },
+  },
+});
