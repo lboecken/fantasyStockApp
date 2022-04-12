fad193223db19ef269d3b8c57e97068cc6519785
Lennart Boecken <lsboecken@gmail.com>
2022-04-12 09:40:15 -0400

    delete index.js in common folder. Not used

--- client/src/modules/common/index.js
+++
@@ -1,9 +0,0 @@
-// import Header from './header/Header';
-// import Button from './button/Button';
-// import Footer from './footer/Footer';
-// import Modal, { useModal } from './modal/Modal';
-
-// export { Button };
-// export { Modal, useModal };
-// export { Footer };
-// export { Header };
